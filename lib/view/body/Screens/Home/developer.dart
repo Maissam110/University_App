0403313bc771f68cdb09957186c2d553312e47ab
Maissam110 <alimaissam18@gmail.com>
2026-08-31 21:08:59 +0500

    developer screen done

--- lib/view/body/Screens/Home/developer.dart
+++ lib/view/body/Screens/Home/developer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DeveloperScreen extends StatelessWidget {
   const DeveloperScreen({super.key});
@@ -47,6 +48,7 @@ class DeveloperScreen extends StatelessWidget {
                   "A Developer who build this university app from scratch to gain the experience and to gain the knowledge, how firebase is work with app in real time.",
                   style: TextStyle(color: color),
                 ),
+                SizedBox(height: 10),
                 Text(
                   "Connect with me",
                   style: TextStyle(
@@ -58,18 +60,147 @@ class DeveloperScreen extends StatelessWidget {
                 Row(
                   children: [
                     //
-                    Card(
-                      color: Theme.of(context).primaryColor,
-                      elevation: 1,
-                      shadowColor: color,
-                      margin: EdgeInsets.all(10),
+                    socialNetwork(
+                      context,
+                      "https://github.com/Maissam110",
+                      "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+                    ),
+                    socialNetwork(
+                      context,
+                      "https://github.com/Maissam110",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE264cyqlcIpV9jQn_DWUOoxyXlu1oTEMExotZtozbPQ&s=10",
+                    ),
+                    socialNetwork(
+                      context,
+                      "https://github.com/Maissam110",
+                      "https://cdn-icons-png.flaticon.com/512/81/81341.png",
                     ),
                   ],
                 ),
               ],
             ),
           ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 1,
+              color: Theme.of(context).primaryColor,
+              shadowColor: color,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //
+                    Text(
+                      "Support & Feedback",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: color,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    ListTile(
+                      leading: Icon(Icons.bug_report, color: color),
+                      title: Text(
+                        "Report a bug",
+                        style: TextStyle(
+                          //
+                          color: color,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        size: 16,
+                        color: color,
+                      ),
+                    ),
+
+                    ListTile(
+                      leading: Icon(Icons.lightbulb, color: color),
+                      title: Text(
+                        "Suffest a feature",
+                        style: TextStyle(
+                          //
+                          color: color,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        size: 16,
+                        color: color,
+                      ),
+                    ),
+
+                    ListTile(
+                      leading: Icon(Icons.star, color: color),
+                      title: Text(
+                        "Rate the app",
+                        style: TextStyle(
+                          //
+                          color: color,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        size: 16,
+                        color: color,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Spacer(),
+          //  Footer things
+          Container(
+            width: double.maxFinite,
+            color: Colors.green[800],
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                //
+                Text(
+                  "University App 2026 | Built with",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                Text(
+                  "Contact us at: support@universityapp.com",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Card socialNetwork(BuildContext context, url, image) {
+    final color = Theme.of(context).colorScheme.secondary;
+    return Card(
+      color: Theme.of(context).primaryColor,
+      elevation: 1,
+      shadowColor: color,
+      child: GestureDetector(
+        onTap: () async {
+          await launchUrl(Uri.parse(url));
+        },
+        child: Container(
+          margin: EdgeInsets.all(10),
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            //
+          ),
+          child: Image.network(image, color: color),
+        ),
       ),
     );
   }
